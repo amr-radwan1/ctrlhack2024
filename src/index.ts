10e181be1c9fbaf1100c785c592f60050a35a434
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-11-09 17:50:41 -0500

    update frontend filess

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { PoseEngine } from "@geenee/bodyprocessors";
 import { Recorder } from "@geenee/armature";
 import { OutfitParams } from "@geenee/bodyrenderers-common";
-import { AvatarRenderer } from "./avatarrenderer";
+import { AnatomyRenderer } from "./anatomyrenderer";
 
 // Engine
 const engine = new PoseEngine();
@@ -15,135 +15,146 @@ const urlParams = new URLSearchParams(window.location.search);
 let rear = urlParams.has("rear");
 // Model map
 const modelMap: {
-    [key: string]: {
-        file: string, avatar: boolean,
-        outfit?: OutfitParams
-    }
+  [key: string]: {
+    file: string;
+    layer: boolean;
+    outfit?: OutfitParams;
+  };
 } = {
-    onesie: {
-        file: "onesie.glb", avatar: false,
-        outfit: {
-            occluders: [/Head$/, /Body/],
-            hidden: [/Eye/, /Teeth/, /Footwear/]
-        }
+  skeleton: {
+    file: "skeleton.glb",
+    layer: false,
+    outfit: {
+      occluders: [],
+      hidden: [],
     },
-    jacket: {
-        file: "jacket.glb", avatar: false,
-        outfit: {
-            occluders: [/Head$/, /Body/],
-            hidden: [/Eye/, /Teeth/, /Bottom/, /Footwear/, /Glasses/]
-        }
-    }
-}
-let model = "onesie";
-let avatar = modelMap["onesie"].avatar;
+  },
+  muscles: {
+    file: "muscles.glb",
+    layer: false,
+    outfit: {
+      occluders: [],
+      hidden: [],
+    },
+  },
+  organs: {
+    file: "organs.glb",
+    layer: false,
+    outfit: {
+      occluders: [],
+      hidden: [],
+    },
+  },
+};
+let model = "skeleton";
+let layer = modelMap["skeleton"].layer;
 
 // Create spinner element
 function createSpinner() {
-    const spinner = document.createElement("div");
-    spinner.className = "boxes";
-    spinner.id = "spinner";
-    for (let i = 0; i < 4; i++) {
-        const box = document.createElement("div");
-        box.className = "box";
-        for (let j = 0; j < 4; j++)
-            box.appendChild(document.createElement("div"));
-        spinner.appendChild(box);
-    }
-    return spinner;
+  const spinner = document.createElement("div");
+  spinner.className = "boxes";
+  spinner.id = "spinner";
+  for (let i = 0; i < 4; i++) {
+    const box = document.createElement("div");
+    box.className = "box";
+    for (let j = 0; j < 4; j++) box.appendChild(document.createElement("div"));
+    spinner.appendChild(box);
+  }
+  return spinner;
 }
 
 async function main() {
-    // Renderer
-    const container = document.getElementById("root");
-    if (!container)
-        return;
-    const renderer = new AvatarRenderer(
-        container, "crop", !rear, modelMap[model].file,
-        avatar ? undefined : modelMap[model].outfit);
-    // Camera switch
-    const cameraSwitch = document.getElementById(
-        "camera-switch") as HTMLButtonElement | null;
-    if (cameraSwitch) {
-        cameraSwitch.onclick = async () => {
-            cameraSwitch.disabled = true;
-            rear = !rear;
-            await engine.setup({ size: { width: 1920, height: 1080 }, rear });
-            await engine.start();
-            renderer.setMirror(!rear);
-            cameraSwitch.disabled = false;
-        }
-    }
-    // Outfit switch
-    const outfitSwitch = document.getElementById(
-        "outfit-switch") as HTMLInputElement;
-    outfitSwitch.checked = avatar;
-    outfitSwitch.onchange = async () => {
-        modelBtns.forEach((btn) => { btn.disabled = true; })
-        outfitSwitch.disabled = true;
+  // Renderer
+  const container = document.getElementById("root");
+  if (!container) return;
+  const renderer = new AnatomyRenderer(
+    container,
+    "crop",
+    !rear,
+    modelMap[model].file,
+    layer ? undefined : modelMap[model].outfit
+  );
+  // Camera switch
+  const cameraSwitch = document.getElementById(
+    "camera-switch"
+  ) as HTMLButtonElement | null;
+  if (cameraSwitch) {
+    cameraSwitch.onclick = async () => {
+      cameraSwitch.disabled = true;
+      rear = !rear;
+      await engine.setup({ size: { width: 1920, height: 1080 }, rear });
+      await engine.start();
+      renderer.setMirror(!rear);
+      cameraSwitch.disabled = false;
+    };
+  }
+  // Layer switch
+  const layerSwitch = document.getElementById(
+    "layer-switch"
+  ) as HTMLInputElement;
+  layerSwitch.checked = layer;
+  layerSwitch.onchange = async () => {
+    modelBtns.forEach((btn) => {
+      btn.disabled = true;
+    });
+    layerSwitch.disabled = true;
+    const spinner = createSpinner();
+    document.body.appendChild(spinner);
+    layer = layerSwitch.checked;
+    await renderer.setLayer(
+      modelMap[model].file,
+      layer ? undefined : modelMap[model].outfit
+    );
+    document.body.removeChild(spinner);
+    modelBtns.forEach((btn) => {
+      btn.disabled = false;
+    });
+    layerSwitch.disabled = false;
+  };
+  // Info button
+  const infoButton = document.getElementById(
+    "info"
+  ) as HTMLButtonElement | null;
+  if (infoButton) {
+    infoButton.onclick = () => {
+      alert(`Current model: ${model}\nLayer mode: ${layer ? "On" : "Off"}`);
+    };
+  }
+  // Model carousel
+  const modelBtns = document.getElementsByName(
+    "model"
+  ) as NodeListOf<HTMLInputElement>;
+  modelBtns.forEach((btn) => {
+    btn.onchange = async () => {
+      if (btn.checked && modelMap[btn.value]) {
+        modelBtns.forEach((btn) => {
+          btn.disabled = true;
+        });
+        layerSwitch.disabled = true;
         const spinner = createSpinner();
         document.body.appendChild(spinner);
-        avatar = outfitSwitch.checked;
-        await renderer.setOutfit(
-            modelMap[model].file,
-            avatar ? undefined : modelMap[model].outfit);
+        model = btn.value;
+        layer = modelMap[model].layer;
+        await renderer.setLayer(
+          modelMap[model].file,
+          layer ? undefined : modelMap[model].outfit
+        );
+        layerSwitch.checked = layer;
         document.body.removeChild(spinner);
-        modelBtns.forEach((btn) => { btn.disabled = false; });
-        outfitSwitch.disabled = false;
-    }
-    // Recorder
-    const safari = navigator.userAgent.indexOf('Safari') > -1 &&
-                   navigator.userAgent.indexOf('Chrome') <= -1
-    const ext = safari ? "mp4" : "webm";
-    const recorder = new Recorder(renderer, "video/" + ext);
-    const recordButton = document.getElementById(
-        "record") as HTMLButtonElement | null;
-    if (recordButton)
-        recordButton.onclick = () => {
-            recorder?.start();
-            setTimeout(async () => {
-                const blob = await recorder?.stop();
-                if (!blob)
-                    return;
-                const url = URL.createObjectURL(blob);
-                const link = document.createElement("a");
-                link.hidden = true;
-                link.href = url;
-                link.download = "capture." + ext;
-                link.click();
-                link.remove();
-                URL.revokeObjectURL(url);
-            }, 10000);
-        };
-    // Model carousel
-    const modelBtns = document.getElementsByName(
-        "model") as NodeListOf<HTMLInputElement>;
-    modelBtns.forEach((btn) => {
-        btn.onchange = async () => {
-            if (btn.checked && modelMap[btn.value]) {
-                modelBtns.forEach((btn) => { btn.disabled = true; })
-                outfitSwitch.disabled = true;
-                const spinner = createSpinner();
-                document.body.appendChild(spinner);
-                model = btn.value;
-                avatar = modelMap[model].avatar;
-                await renderer.setOutfit(
-                    modelMap[model].file,
-                    avatar ? undefined : modelMap[model].outfit);
-                outfitSwitch.checked = avatar;
-                document.body.removeChild(spinner);
-                modelBtns.forEach((btn) => { btn.disabled = false; });
-                outfitSwitch.disabled = false;
-            }
-        };
-    });
-    // Initialization
-    await Promise.all([
-        engine.addRenderer(renderer),
-        engine.init({ token: token })
-    ]);
-    await engine.setup({ size: { width: 1920, height: 1080 }, rear });
-    await engine.start();
-    document.getElementById("loadui")?.remove();
+        modelBtns.forEach((btn) => {
+          btn.disabled = false;
+        });
+        layerSwitch.disabled = false;
+      }
+    };
+  });
+  // Initialization
+  await Promise.all([
+    engine.addRenderer(renderer),
+    engine.init({ token: token }),
+  ]);
+  await engine.setup({ size: { width: 1920, height: 1080 }, rear });
+  await engine.start();
+  document.getElementById("loadui")?.remove();
 }
 main();
